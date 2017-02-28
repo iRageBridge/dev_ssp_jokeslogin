841ba8fb511fc4ca09e7acd47879edaf3686f905
Simon Standbridge <simonstandbridge@gmail.com>
2017-02-28 12:38:08 +0000

    added index page

--- dev_ssp_jokeslogin/routes/index.js
+++ dev_ssp_jokeslogin/routes/index.js
@@ -3,17 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/login', function(req, res, next) {
-  if(!req.session.hasOwnProperty('passWord')){
-    req.session.passWord = 'simonstan';
-  }
   res.render('login');
 });
 
 router.post('/login', function(req,res,next){
   var username = req.body.uName;
   var password = req.body.pWord;
-  
-  if(username.length == 0 || password.length == 0 || password != req.session.password){
+  if(username.length == 0 || password.length == 0 || password != 'simonstan'){
     res.redirect('/login');
   }
   else{
@@ -25,5 +21,20 @@ router.post('/login', function(req,res,next){
 
 router.get('/', function(req,res,next){
   res.render('index')
-})
+});
+
+router.post('/', function(req, res, next){
+  var joke = {
+    joke: req.body.joke,
+    author: 'simon',
+    data: Date()
+  };
+  if(!req.session.hasOwnProperty('jokeTest')){
+    req.session.jokeTest = [];
+  }
+  else{
+    req.session.jokeTest.push(joke);
+    res.redirect('/');
+  }
+});
 module.exports = router;
