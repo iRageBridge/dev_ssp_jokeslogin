656a0e48ed57f19e86886cea33f2f9a02da38d65
Simon Standbridge <simonstandbridge@gmail.com>
2017-02-28 12:27:15 +0000

    added password

--- dev_ssp_jokeslogin/routes/index.js
+++ dev_ssp_jokeslogin/routes/index.js
@@ -1,9 +1,29 @@
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/login', function(req, res, next) {
+  if(!req.session.hasOwnProperty('passWord')){
+    req.session.passWord = 'simonstan';
+  }
+  res.render('login');
 });
 
+router.post('/login', function(req,res,next){
+  var username = req.body.uName;
+  var password = req.body.pWord;
+  
+  if(username.length == 0 || password.length == 0 || password != req.session.password){
+    res.redirect('/login');
+  }
+  else{
+    //req.session.uName = username;
+    //req.session.pWord = password;
+    res.redirect('/');
+  }
+});
+
+router.get('/', function(req,res,next){
+  res.render('index')
+})
 module.exports = router;
