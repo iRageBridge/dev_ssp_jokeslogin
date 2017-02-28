684dd0548ab1682ce6a2f42db53bf60b60299b59
Simon Standbridge <simonstandbridge@gmail.com>
2017-02-28 11:35:12 +0000

    setup app.js

--- dev_ssp_jokeslogin/app.js
+++ dev_ssp_jokeslogin/app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session')
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,9 +20,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session(expressSession));
 app.use('/', index);
-app.use('/users', users);
+
+var expressSession={
+  secret:'aSecret',
+  resave: false,
+  saveUninitialized: false
+};
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
